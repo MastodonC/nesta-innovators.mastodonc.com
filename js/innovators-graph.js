0b1f207e884cc4c129f4d4779b9a6aa45dd46794
Bruce Durling <bruce@mastodonc.com>
2014-03-29 23:31:40 +0000

    Colour subgraph when clicking on node.

--- js/innovators-graph.js
+++ js/innovators-graph.js
@@ -1,21 +1,92 @@
+// Add a method to the graph model that returns an
+// object with every neighbors of a node inside:
+sigma.classes.graph.addMethod('neighbors', function(nodeId) {
+    var k,
+        neighbors = {},
+        index = this.allNeighborsIndex[nodeId] || {};
+
+    for (k in index) {
+      neighbors[k] = this.nodesIndex[k];
+    }
+
+    return neighbors;
+});
+
 var innovators = function() {
-  var current;
-  var graph = function(jsonFile, container) {
-    sigma.parsers.json(jsonFile,
-            {container: container},
-            function(obj) {
-
-              obj.bind('clickNode', function(e) {
-                  console.log(e.data.node.label);
-                  var github = e.data.node.label;
-                  $("#follower .twitter").html('<a href="http://github.com/' + github + '"><i class="icon-github"/> ' + github + '</a>');
-              });
-
-              current = obj.graph;
-
-            });
-   
-  }
+    var current;
+    var graph = function(jsonFile, container) {
+        sigma.parsers.json(jsonFile,
+                           {container: container},
+                           function(obj) {
+                               current = obj.graph;
+                               
+                               obj.bind('clickNode', function(e) {
+                                   console.log(e.data.node.label);
+                                   var github = e.data.node.label;
+                                   $("#follower .twitter").html('<a href="http://github.com/' + github + '"><i class="icon-github"/> ' + github + '</a>');
+                               });
+                               
+                               
+                               // sub-graph colouring
+                               
+                               // We first need to save the original colors of our
+                               // nodes and edges, like this:
+                               obj.graph.nodes().forEach(function(n) {
+                                   n.originalColor = n.color;
+                               });
+                               obj.graph.edges().forEach(function(e) {
+                                   e.originalColor = e.color;
+                               });
+                               
+                               // When a node is clicked, we check for each node
+                               // if it is a neighbor of the clicked one. If not,
+                               // we set its color as grey, and else, it takes its
+                               // original color.
+                               // We do the same for the edges, and we only keep
+                               // edges that have both extremities colored.
+                               obj.bind('clickNode', function(e) {
+                                   var nodeId = e.data.node.id,
+                                       toKeep = obj.graph.neighbors(nodeId);
+                                   toKeep[nodeId] = e.data.node;
+                                   
+                                   obj.graph.nodes().forEach(function(n) {
+                                       if (toKeep[n.id]) {
+                                           n.color = n.originalColor;
+                                       } else {
+                                           n.color = '#eee';
+                                       }
+                                   });
+                                   
+                                   obj.graph.edges().forEach(function(e) {
+                                       if (toKeep[e.source] && toKeep[e.target]) {
+                                           e.color = e.originalColor;
+                                       } else {
+                                           e.color = '#eee';
+                                       }
+                                   });
+                                   
+                                   // Since the data has been modified, we need to
+                                   // call the refresh method to make the colors
+                                   // update effective.
+                                   obj.refresh();
+                               });
+                               
+                               // When the stage is clicked, we just color each
+                               // node and edge with its original color.
+                               obj.bind('clickStage', function(e) {
+                                   obj.graph.nodes().forEach(function(n) {
+                                       n.color = n.originalColor;
+                                   });
+                                   
+                                   obj.graph.edges().forEach(function(e) {
+                                       e.color = e.originalColor;
+                                   });
+                                   
+                                   // Same as in the previous event:
+                                   obj.refresh();
+                               });
+                           });
+    }
 
   var clear = function() {
     if (current) {
