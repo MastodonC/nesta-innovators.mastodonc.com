3b70272c6b2a2d40c6117f37f8cf087e03551de3
Bruce Durling <bruce@mastodonc.com>
2014-03-30 12:15:35 +0100

    Shortened locations and fixed chart height.

--- js/location.js
+++ js/location.js
@@ -1,16 +1,16 @@
 var width = 500;
 var height = 500;
 
-var chart = d3.select("#location.chart");
-//	 .attr("width", width)
-//	 .attr("height", height);
+var chart = d3.select("#location.chart")
+	 .attr("width", width)
+	 .attr("height", height/2);
 
 d3.json("data/by-location.json", function(error, json) {
     if (error) return console.warn(error);
 
     data2 = json;
     
-    var barHeight = height/data2.length;
+    var barHeight = height / 2 / data2.length;
     
     var x = d3.scale.linear()
 	    .domain([0, d3.max(data2, function(d) { return d.Count; })])
